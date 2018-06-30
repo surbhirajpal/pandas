10cf773e3c695a9da8cbabd0b35f56af20080d6d
surbhirajpal <39900565+surbhirajpal@users.noreply.github.com>
2018-06-30 18:21:13 +0530

    Add files via upload

---
+++ pandassss.py
@@ -0,0 +1,36 @@
+import pandas as pd
+import numpy as np
+#Q1
+
+data={'name':['surbhi'],'age':[20],'mail id':['surbhirajpal88@gmail.com'],'phone no.':[8872296040]}
+df=pd.DataFrame(data)
+print(df)
+print("\n \n after adding information of my friend ")
+df.loc[1]=['abc',22,'abc98@gmail.com',7647864782]
+print(df)
+
+
+#Q2
+data=pd.read_csv("data.csv")
+df=pd.DataFrame(data)
+print("first five rows of data frame are")
+print(df.head(5))
+print("first ten rows of data frame are")
+print(df.head(10))
+print("basic statics are")
+print("axes\n"+str(df.axes))
+print("datatypes\n"+str(df.dtypes))
+print("dimension\n"+str(df.ndim))
+print("shape\n"+str(df.shape))
+print("size\n"+str(df.size))
+print("last 5 rows are")
+print(df.tail(5))
+
+x=df.loc[:,'Location']
+print(x)
+print("basic statics are")
+print("axes\n"+str(x.axes))
+print("datatypes\n"+str(x.dtypes))
+print("dimension\n"+str(x.ndim))
+print("shape\n"+str(x.shape))
+print("size\n"+str(x.size))
